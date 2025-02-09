18dc8d7af5467d3075b84de6f506ea219a4b8c8e
Fernando <mhfernando778@gmail.com>
2025-02-09 13:01:04 -0500

    Task 2 - Sales Tax Cpmputation using function expression

--- cc_6.js
+++ cc_6.js
@@ -6,3 +6,13 @@ return profit;
 
 console.log(`Profits Earned 1: $${calculateProfit(20, 30, 100)}`); // logging profits earned with given numbers
 console.log(`Profits Earned 2: $${calculateProfit(50, 70, 200)}`); // logging profits earned with given numbers
+
+
+// Task 2 - Sales Tax Computation //
+const calculateSalesTax = function(amount, taxRate) {
+    let salesTax = Math.floor(amount * taxRate);
+    return salesTax;
+};
+
+console.log(`Calculated Sales Tax 1: $${calculateSalesTax(100, .07)}`);
+console.log(`Calculated Sales Tax 2: $${calculateSalesTax(500, .1)}`);
