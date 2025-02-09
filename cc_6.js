39abd77159fcb51140fd80878bcf9a1736d887d7
Fernando <mhfernando778@gmail.com>
2025-02-09 12:42:28 -0500

    Task 1 - added comments

--- cc_6.js
+++ cc_6.js
@@ -1,8 +1,8 @@
 // Task 1 - Business Profit Calculations //
-function calculateProfit(costPrice, sellingPrice, unitsSold) {
-let profit = ((sellingPrice - costPrice) * unitsSold);
+function calculateProfit(costPrice, sellingPrice, unitsSold) { // starting a function to calcualte profit
+let profit = ((sellingPrice - costPrice) * unitsSold); // stating the equation
 return profit;
 };
 
-console.log(`Profits Earned 1: $${calculateProfit(20, 30, 100)}`);
-console.log(`Profits Earned 2: $${calculateProfit(50, 70, 200)}`);
+console.log(`Profits Earned 1: $${calculateProfit(20, 30, 100)}`); // logging profits earned with given numbers
+console.log(`Profits Earned 2: $${calculateProfit(50, 70, 200)}`); // logging profits earned with given numbers
